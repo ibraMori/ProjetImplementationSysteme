dd200c5781b80a406ea61fa84bd1e38ce63f84da
ibraMori <ibramoricam@gmail.com>
2026-04-26 09:19:20 -0400

    updated bd Table

---
+++ code/welcomeCanada(v1)/UnitTestApp/UnitTest1.cs
@@ -0,0 +1,66 @@
+using welcomeCanada;
+using welcomeCanada.Services;
+using System.Data.SqlClient;
+using welcomeCanada.Database;
+namespace UnitTestApp
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+        [Test]
+        public void Connect_ValidUser()
+        {
+            string email = "marc@gmail.com";
+            string mdp = "marc";
+
+            AuthService authService = new AuthService();
+            bool connexion = authService.Connexion(email, mdp);
+            Assert.AreEqual(true, connexion);
+        }
+        [Test]
+        public void Multiply_NormalCase()
+        {
+            int a = 5;
+            int b = 10;
+            int result = 50;
+
+            TestCalculation calculation = new TestCalculation();
+            int testResult = calculation.multiply(a, b);
+            Assert.AreEqual(result, testResult);
+
+        }
+        [Test]
+        public void Multiply_NormalCase2()
+        {
+            int a = 87;
+            int b = -10;
+            int result = -870;
+
+            TestCalculation calculation = new TestCalculation();
+            int testResult = calculation.multiply(a, b);
+            Assert.AreEqual(result, testResult);
+
+        }
+        [Test]
+        public void Multiply_BoundaryCase()
+        {
+            int a = 8878787;
+            int b = 9222788;
+            int result = int.MaxValue;
+
+            TestCalculation calculation = new TestCalculation();
+            int testResult = calculation.multiply(a, b);
+            Assert.AreEqual(result, testResult);
+
+        }
+    }
+}
---
+++ code/welcomeCanada(v1)/welcomeCanada/TestCalculation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace welcomeCanada
+{
+    public class TestCalculation
+    {
+        public int multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
